f00744430663c9eaa52b3b7cc55389c16ab9f3b9
noha_m_elmandoh@outlook.com <noha_m_elmandoh@outlook.com>
2021-07-15 13:40:55 +0200

    add arabic style

--- resources/views/front/partials/style.blade.php
+++ resources/views/front/partials/style.blade.php
@@ -7,7 +7,10 @@
  @if ( Config::get('app.locale') == 'en')
  <link rel="stylesheet" href="{{asset('front/style.css')}}">
 @elseif ( Config::get('app.locale') == 'ar' )
-<link rel="stylesheet" href="{{asset('front/style.css')}}">
+<link rel="stylesheet" href="{{asset('front/css/style-RTL.css.css')}}">
+<link rel="stylesheet" href="{{asset('front/css/bootstrap-rtl.css')}}">
+
+<link href="https://fonts.googleapis.com/css?family=Cairo:300,400,700,900" rel="stylesheet">
 @endif
 
 
